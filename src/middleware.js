1c96f7958418f3f7d59d313d871b0e9ea898e681
David Stockley <magicspon@gmail.com>
2024-11-14 12:36:06 +0000

    log

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware() {
+	console.log("[run middleware]");
+
 	return NextResponse.next();
 }
 
