fe1bdedf21cd4f4a453a9d835e5ae28e438e7b27
David Stockley <magicspon@gmail.com>
2024-11-14 12:31:25 +0000

    go go go

--- src/pages/[...slug].js
+++ src/pages/[...slug].js
@@ -14,8 +14,6 @@ const geistMono = localFont({
 });
 
 export default function Slug({ posts, data }) {
-	console.log({ posts, a: data?.waffle });
-
 	return (
 		<div
 			className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}>
